d2e47e7562ff1b95d7779cd2cef2b74c49c4ad82
Bernat Ferragut <elbernat@gmail.com>
2017-04-13 09:54:57 -0300

    new Recipe with ingredients created

--- recipes/recipe.service.ts
+++ recipes/recipe.service.ts
@@ -3,8 +3,19 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
 
     recipes: Recipe[]= [
-    new Recipe('Vegan1', 'Description1: Vegan1 is ...', './assets/images/recipe.jpg'),
-    new Recipe('Vegan2', 'Description2: Vegan2 is ...', './assets/images/recipe.jpg'),
+    new Recipe
+        ('Vegan1',
+        'Description1: Vegan1 is ...',
+        './assets/images/recipe.jpg',
+         []
+         ),
+
+    new Recipe
+        ('Vegan2',
+        'Description2: Vegan2 is ...', 
+        './assets/images/recipe.jpg',
+         []
+         ),
      ];
 
     getRecipe() {
