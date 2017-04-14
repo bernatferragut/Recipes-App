1ac64ab6ececfaa820cda654bfb49bc140fef191
Bernat Ferragut <elbernat@gmail.com>
2017-04-14 09:50:47 -0300

    main paths achieved

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -5,9 +5,9 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 // Routes Creation
 const appRoutes: Routes = [
-    { path: '', redirectTo: './recipes'},
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent },
-    { path: 'shopping-list', component: ShoppingListComponent }
+    { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
 @NgModule({
