e11bd4fc79e3fffeb0be3529c1cbf7571f4d957e
Bernat Ferragut <elbernat@gmail.com>
2017-04-14 09:29:48 -0300

    Routes Module Config1

---
+++ app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+    {},
+    {},
+    {},
+    {}
+];
+
+@NgModule({
+
+})
+export class AppRoutingModule {
+
+}
